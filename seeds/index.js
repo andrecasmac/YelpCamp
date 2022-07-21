808010681538b46b3012f5fed3748d6669929691
Andre Castillo <a01254180@itesm.mx>
2022-07-21 14:15:37 -0700

    Implemented basic authorization

--- seeds/index.js
+++ seeds/index.js
@@ -22,6 +22,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '62d8cf71f1f5aef12c3e56f9',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
